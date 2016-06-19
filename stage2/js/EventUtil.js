192017c0daecc4e94570c4668c25ee0942d4f0ed
Nutlee <lixuanyy@gmail.com>
2016-06-19 21:44:42 +0800

    2.11 基本UI,EventUtil.js 基本的绑定方法。

---
+++ stage2/js/EventUtil.js
@@ -0,0 +1,31 @@
+var EventUtil = {
+	addHandler: function(element,type,handler) {
+		if (element.addEventListener) {
+			element.addEventListener(type,handler,false);
+		} else if (element.attachEvent) {
+			element.attachEvent("on"+type,handler);
+		} else {
+			element["on"+type]=handler;
+		}
+	},
+	getEvent: function(event) {
+		return event ? event :window.event;
+	},
+	getTarget: function(event) {
+		return event.target || event.scrElement;
+	},
+	getCharCode: function(event) {
+		if (typeof event.charCode == "number") {
+			return event.charCode;
+		} else {
+			return event.keyCode;
+		}
+	},
+	preventDefault: function(event) {
+		if (event.preventDefault) {
+			event.preventDefault();
+		} else {
+			event.returnValue = false;
+		}
+	}
+};
