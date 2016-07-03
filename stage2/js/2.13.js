42772119be80214f9e77b4c0c1298845b89f52aa
Nutlee <lixuanyy@gmail.com>
2016-07-04 00:10:04 +0800

    2.13 add Node and delete Node function complete.

--- stage2/js/2.13.js
+++ stage2/js/2.13.js
@@ -1,6 +1,6 @@
 EventUtil.addHandler($('#root'),"click",function(){
 	var event=EventUtil.getEvent(event),
-		target=EventUtil.getTarget(event);
+		target=EventUtil.getTarget(event),
 		cldNode=target.getElementsByTagName('ul')[0],
 		allNode = this.parentNode.getElementsByTagName('li');
 	if (target.nodeName.toLowerCase() === 'li') {
@@ -10,7 +10,7 @@ EventUtil.addHandler($('#root'),"click",function(){
 				removeClass(iNode,'focus');
 			}
 		};
-		target.className += 'focus';
+		target.className += ' focus';
 	};
 	if (cldNode) {
 		if (elementHasClassName(cldNode,'hide')) {
@@ -19,6 +19,39 @@ EventUtil.addHandler($('#root'),"click",function(){
 		} else {
 			removeClass(cldNode,'show');
 			cldNode.className += 'hide';			
+		}
+	};
+});
+EventUtil.addHandler($('#operate'),"click",function(){
+	var event=EventUtil.getEvent(event),
+		target=EventUtil.getTarget(event),
+		focusNode=getElementsByClassName('focus')[0];
+	if (target.id === 'addnode') {
+		var addNodeValue=$('#newnodevalue').value.trim();
+		if (focusNode) {
+			if (addNodeValue) {
+				var branch=focusNode.getElementsByTagName('ul')[0];
+				if (branch) {
+					branch.innerHTML += '<li>'+addNodeValue+'</li>';
+				} else {
+					focusNode.innerHTML += '<ul><li>'+addNodeValue+'</li></ul>';
+				};
+			} else {
+				alert('请输入插入的节点值');
+			};
+		} else {
+			alert('请先选中节点');
+		};	
+	};
+	if (target.id === 'delnode') {
+		if (focusNode) {
+			if (elementHasClassName(focusNode,'root')) {
+				alert('根节点不能删除')
+			} else {
+				focusNode.parentNode.removeChild(focusNode);
+			};
+		} else {
+			alert('请选中节点');
 		}
 	};
 });
